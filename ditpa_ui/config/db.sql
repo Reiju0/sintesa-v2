a6d4d2b0daffd260499b17a6cb8ea8f526324fb1
Reiju0 <58357126+Reiju0@users.noreply.github.com>
2023-09-04 22:24:26 +0700

    Delete ditpa_ui directory

--- ditpa_ui/config/db.sql
+++
@@ -1,7 +0,0 @@
-create database tkd_2023;
-
-use tkd_2023;
-
-show tables;
-
--- DATABASE_URL='mysql://rolshoedutjjyqpe299x:pscale_pw_xEjFgU4tRmztwxAca4FUzX1P6hzyABBL6GKlCAUDB2h@aws.connect.psdb.cloud/tkd-2023?sslaccept=strict'
